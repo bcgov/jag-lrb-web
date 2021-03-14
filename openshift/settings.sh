57b286937b992a69ffd3c42755d7f38e968d9194
Emiliano Suñé <emiliano.sune@gmail.com>
2021-03-14 13:59:45 -0700

    Tweaks to settings.sh, TestConnections.txt

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -3,6 +3,6 @@ export PROJECT_NAMESPACE="6b08a3"
 export GIT_URI="https://github.com/bcgov/jag-lrb-web.git"
 export GIT_REF="main"
 
-export skip_git_overrides="solr-base-build.yaml"
+export skip_git_overrides="solr-base-build.yaml backup-build.yaml"
 
 export ignore_templates=""

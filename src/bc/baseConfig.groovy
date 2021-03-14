e792f3ddf5894707d72e6c33e8a9b04c5505230e
Emiliano Suñé <emiliano.sune@gmail.com>
2021-03-14 14:55:52 -0700

    Added pipelines

---
+++ src/bc/baseConfig.groovy
@@ -0,0 +1,19 @@
+    
+package bc;
+
+class baseConfig {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
+
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = '6b08a3'
+
+  // Apps - Listed in the order they should be built
+  public static final String[] BUILD_APPS = ['drupal-proxy', 'solr']
+
+  // Apps - Listed in the order they should be deployed
+  public static final String[] DEPLOY_APPS = ['drupal-db', 'drupal-proxy', 'drupal', 'solr', 'backup-mariadb']
+}
